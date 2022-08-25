59e58180a20c06b02a03a8da0ccaf7f85e445fe2
hj522 <jjungvvv@naver.com>
2022-08-25 23:23:52 +0900

    Coding Test

---
+++ Test/src/Backjun/Test02.java
@@ -0,0 +1,15 @@
+package Backjun;
+
+import java.util.Scanner;
+
+// 10926번
+public class Test02 {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		String ID = scanner.nextLine();
+
+		System.out.println(ID+"??!");		
+	}
+}
