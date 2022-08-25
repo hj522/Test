2022b3e3663d6d23a9a01c2521a834cce3b3aaef
hj522 <jjungvvv@naver.com>
2022-08-25 23:22:46 +0900

    No.10998

---
+++ Test/src/Backjun/Test01.java
@@ -0,0 +1,16 @@
+package Backjun;
+
+import java.util.Scanner;
+
+// 10998번
+public class Test01 {
+	public static void main(String[] args) {
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		int A = scanner.nextInt();
+		int B = scanner.nextInt();
+		
+		System.out.println(A*B);
+	}
+}
